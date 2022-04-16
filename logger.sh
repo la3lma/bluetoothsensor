52d752868432eff860d13e9d61b199ef16906181
Bjørn Remseth <la3lma@gmail.com>
2022-04-16 21:40:24 +0200

    Simple logger for testing resilience

---
+++ logger.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TRUE=/usr/bin/true
+LOGFILE=/Volumes/BigTemp/btlog/thelog.txt
+SLEEP_INTERVAL_IN_SECONDS=40
+
+touch  "${LOGFILE}"
+
+while $TRUE ; do
+    TIMESTAMP="$(date)"
+    echo "Trying at ${TIMESTAMP}"
+    echo "== ${TIMESTAMP}">> "${LOGFILE}"
+    curl   --connect-timeout 40 http://10.0.0.35/bluetooth-device-report >> "${LOGFILE}"
+    sleep $SLEEP_INTERVAL_IN_SECONDS
+done    
