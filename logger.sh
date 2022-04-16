3d8a374a81736fc4e9d0254ca372c6f2d123ea76
Bjørn Remseth <la3lma@gmail.com>
2022-04-16 21:42:18 +0200

    Fixing a few bugs

--- logger.sh
+++ logger.sh
@@ -9,7 +9,7 @@ touch  "${LOGFILE}"
 while $TRUE ; do
     TIMESTAMP="$(date)"
     echo "Trying at ${TIMESTAMP}"
-    echo "== ${TIMESTAMP}">> "${LOGFILE}"
+    echo "\n== ${TIMESTAMP}">> "${LOGFILE}"
     curl   --connect-timeout 40 http://10.0.0.35/bluetooth-device-report >> "${LOGFILE}"
     sleep $SLEEP_INTERVAL_IN_SECONDS
 done    
