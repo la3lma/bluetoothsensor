f2b6d033828387830593741d1f7aa4e5fc8a3da1
Bjørn Remseth <la3lma@gmail.com>
2022-04-17 15:16:35 +0200

    Some database thingies

--- logger.sh
+++ logger.sh
@@ -2,14 +2,21 @@
 
 TRUE=/usr/bin/true
 LOGFILE=/Volumes/BigTemp/btlog/thelog.txt
-SLEEP_INTERVAL_IN_SECONDS=40
+SLEEP_INTERVAL_IN_SECONDS=10
+
+DEVICES="10.0.0.36 10.0.0.35"
 
 touch  "${LOGFILE}"
 
 while $TRUE ; do
-    TIMESTAMP="$(date)"
-    echo "Trying at ${TIMESTAMP}"
-    echo "\n== ${TIMESTAMP}">> "${LOGFILE}"
-    curl   --connect-timeout 40 http://10.0.0.35/bluetooth-device-report >> "${LOGFILE}"
+
+    for DEVICE in $DEVICES ; do
+       QUERY_URL="http://${DEVICE}/bluetooth-device-report"	
+       TIMESTAMP="$(date)"
+       echo "Trying '$QUERY_URL' at ${TIMESTAMP}"
+       echo >> "${LOGFILE}"    
+       echo "== $DEVICE  ${TIMESTAMP}">> "${LOGFILE}"
+       curl   --connect-timeout 30 ${QUERY_URL} >> "${LOGFILE}"
+    done
     sleep $SLEEP_INTERVAL_IN_SECONDS
 done    
