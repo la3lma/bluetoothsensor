4c5bbc868f6a8840b6f6ddd71be6091e69fee50e
Bjørn Remseth <la3lma@gmail.com>
2022-04-18 17:17:57 +0200

    Cleaning up. Very sparse verification of this json parsing, but at this moment it will have to pass.

--- btcrawl/test/bt_report_parser/ParseBtReport_test.go
+++ btcrawl/test/bt_report_parser/ParseBtReport_test.go
@@ -2,35 +2,20 @@ package bt_report_parser
 
 import (
 	"btcrawl/internal/bt_report_parser"
-	"encoding/json"
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func TestParseScannerId(t *testing.T) {
-	scannerIdJson := `{"wifiMAC":"7C:9E:BD:4B:2F:1C"}`
-	var scannerid bt_report_parser.ScannerId
-	json.Unmarshal([]byte(scannerIdJson), &scannerid)
-	assert.Equal(t, "7C:9E:BD:4B:2F:1C", scannerid.WifiMAC)
-}
-
 func TestParseBtReport(t *testing.T) {
 	var jsonBytes []byte
 	jsonBytes, err := os.ReadFile("../testdata/example-bt-log.json")
-	check(err)
-	// TODO: Drop this print once all the parts has been verified
-	fmt.Print(string(jsonBytes))
+	assert.NoError(t, err)
 
 	bleScan, err := bt_report_parser.ParseBtReport(jsonBytes)
+	assert.NoError(t, err)
 
-	// TODO: Add more assertions
+	// TODO: Add more assertions, ... or not
 	assert.Equal(t, "7C:9E:BD:4B:2F:1C", bleScan.ScannerID.WifiMAC)
+	assert.Equal(t, -37, bleScan.BleReports[0].Rssi)
 }
