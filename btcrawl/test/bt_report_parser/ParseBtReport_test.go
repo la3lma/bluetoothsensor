9041fa28a8dab2bc640d1df9fa53f41a4451a8d4
Bjørn Remseth <la3lma@gmail.com>
2022-04-18 16:38:11 +0200

    Getting ready to roll

--- btcrawl/test/bt_report_parser/ParseBtReport_test.go
+++ btcrawl/test/bt_report_parser/ParseBtReport_test.go
@@ -2,13 +2,24 @@ package bt_report_parser
 
 import (
 	"btcrawl/internal/bt_report_parser"
+	"fmt"
+	"os"
 	"testing"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func TestParseBtReport(t *testing.T) {
-	// TODO: Read everything from this file. "../testdata/example-bt-log.json"
+	json, err := os.ReadFile("../testdata/example-bt-log.json")
+	check(err)
+	fmt.Print(string(json))
+
 	want := "Hello, world."
-	if got := bt_report_parser.ParseBtReport(""); got != want {
+	if got := bt_report_parser.ParseBtReport(string(json)); got != want {
 		t.Errorf("Hello() = %q, want %q", got, want)
 	}
 }
