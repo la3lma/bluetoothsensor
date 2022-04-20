1d620e66b2aa523bdaff249f9449203815d17820
Bjørn Remseth <la3lma@gmail.com>
2022-04-20 18:39:34 +0200

    Refaccdtor into separate method for getting in memory db

--- btcrawl/test/persistence/Database_test.go
+++ btcrawl/test/persistence/Database_test.go
@@ -9,14 +9,23 @@ import (
 
 func TestPing(t *testing.T) {
 	var db *sqlx.DB
-
-	// exactly the same as the built-in
-	db, err := sqlx.Open("sqlite3", ":memory:")
-
-	// from a pre-existing sql.DB; note the required driverName
-	// db, err := sqlx.NewDb(sql.Open("sqlite3", ":memory:"), "sqlite3")
+	db, err := getInMemoryDb(t)
 	assert.NoError(t, err, "Couldn't make in  memory db")
 
 	// force a connection and test that it worked
 	err = db.Ping()
+	assert.NoError(t, err, "Couldn't ping to memory db")
+
+}
+
+func getInMemoryDb(t *testing.T) (*sqlx.DB, error) {
+	var db *sqlx.DB
+
+	// exactly the same as the built-in
+	db, err := sqlx.Open("sqlite3", ":memory:")
+	return db, err
+
+	// from a pre-existing sql.DB; note the required driverName
+	// db, err := sqlx.NewDb(sql.Open("sqlite3", ":memory:"), "sqlite3")
+
 }
