c0eb36cbcca63254348588259b134cf93ed88f4d
Bjørn Remseth <la3lma@gmail.com>
2022-04-20 18:35:46 +0200

    Add necessary import to load driver

---
+++ btcrawl/test/persistence/Database_test.go
@@ -0,0 +1,22 @@
+package persistence
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	var db *sqlx.DB
+
+	// exactly the same as the built-in
+	db, err := sqlx.Open("sqlite3", ":memory:")
+
+	// from a pre-existing sql.DB; note the required driverName
+	// db, err := sqlx.NewDb(sql.Open("sqlite3", ":memory:"), "sqlite3")
+	assert.NoError(t, err, "Couldn't make in  memory db")
+
+	// force a connection and test that it worked
+	err = db.Ping()
+}
