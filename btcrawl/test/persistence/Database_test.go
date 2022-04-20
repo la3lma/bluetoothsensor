a2619f56ba2b1d4dce0f53fd79eeaf63576efd38
Bjørn Remseth <la3lma@gmail.com>
2022-04-20 19:18:25 +0200

    Yay, reading from the database. Now we're on a roll!

--- btcrawl/test/persistence/Database_test.go
+++ btcrawl/test/persistence/Database_test.go
@@ -2,6 +2,8 @@ package persistence
 
 import (
 	"btcrawl/internal/persistence"
+	"database/sql"
+	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
@@ -20,4 +22,25 @@ func TestReadDDLFromFile(t *testing.T) {
 	db, err := persistence.NewInMemoryDb(t)
 	err = persistence.InjectDatabaseModel(db)
 	assert.NoError(t, err, "Couldn't load schema")
+}
+
+// This is essentially a smoketest to check if we can read what we want to read from the
+// database.  We're not really interested in the scanner type at this point.
+func TestGetScannerType(t *testing.T) {
+	db, err := persistence.NewInMemoryDb(t)
+	err = persistence.InjectDatabaseModel(db)
+	assert.NoError(t, err, "Couldn't load schema")
+
+	rows, err := db.Query("SELECT id, name, description  FROM scanner_type")
+	assert.NoError(t, err, "Scanner type query failed")
+	// iterate over each row
+	for rows.Next() {
+		var id int
+		// note that city can be NULL, so we use the NullString type
+		var name sql.NullString
+		var description sql.NullString
+		err = rows.Scan(&id, &name, &description)
+		assert.NoError(t, err, "Could not read row")
+		fmt.Println("Just read ", id, ", ", name, ", ", description)
+	}
 }
