03579bd4fbf64511fe310da2667b55927942dba1
Bjørn Remseth <la3lma@gmail.com>
2022-04-20 18:47:32 +0200

    Read in schema definition into in-memory database

--- btcrawl/test/persistence/Database_test.go
+++ btcrawl/test/persistence/Database_test.go
@@ -4,19 +4,9 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"testing"
 )
-
-func TestPing(t *testing.T) {
-	var db *sqlx.DB
-	db, err := getInMemoryDb(t)
-	assert.NoError(t, err, "Couldn't make in  memory db")
-
-	// force a connection and test that it worked
-	err = db.Ping()
-	assert.NoError(t, err, "Couldn't ping to memory db")
-
-}
 
 func getInMemoryDb(t *testing.T) (*sqlx.DB, error) {
 	var db *sqlx.DB
@@ -27,5 +17,23 @@ func getInMemoryDb(t *testing.T) (*sqlx.DB, error) {
 
 	// from a pre-existing sql.DB; note the required driverName
 	// db, err := sqlx.NewDb(sql.Open("sqlite3", ":memory:"), "sqlite3")
+}
 
+func TestReadDDLFromFile(t *testing.T) {
+	schema, err := ioutil.ReadFile("../../../schema.sql")
+	assert.NoError(t, err, "Couldn't read sql schema def")
+	db, err := getInMemoryDb(t)
+	assert.NoError(t, err, "Couldn't make in  memory db")
+	_, err = db.Exec(string(schema))
+	assert.NoError(t, err, "Couldn't load schema")
+}
+
+func TestPing(t *testing.T) {
+	var db *sqlx.DB
+	db, err := getInMemoryDb(t)
+	assert.NoError(t, err, "Couldn't make in  memory db")
+
+	// force a connection and test that it worked
+	err = db.Ping()
+	assert.NoError(t, err, "Couldn't ping to memory db")
 }
